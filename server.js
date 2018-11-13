6511865327762f21003fc5c163fa67d63f18f1c6
Stoic Dev <stoicwebdev@gmail.com>
2018-11-13 07:07:35 -0500

    cors enabled

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.get('/', (req, res) => {
 res.json({ok: true});
 });
 
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 module.exports = {app};
