f7d00b8c3576ae254431f25fc084b81f5ddee6b1
Stoic Dev <stoicwebdev@gmail.com>
2018-11-19 09:42:08 -0500

    findoneandupdate working

--- controllers/daysRouter.js
+++ controllers/daysRouter.js
@@ -36,45 +36,35 @@ router.get('/', jwtAuth, (req, res) => {
 });
 
 
-// POST - Create a new Day
-// No validation needed?
-// IMPROVEMENT - But if nothing is sent in the one of the lists, don't save that in the document?
+// POST - Create a new Day or Update existing
 router.post('/', jwtAuth, (req, res) => {
   console.log("req.user", req.user)
-  Day.create({
-    user: req.user,
-    date: req.body.date,
-    grateful: req.body.grateful,
-    greatness: req.body.greatness,
-    affirmation: req.body.affirmation
-  })
+  console.log('req.body', req.body)
+  //query for the day... unique day _id
+  let query = {'_id': (req.body._id || new mongoose.mongo.ObjectID())};
+  console.log('query', query);
+  let updates = {
+    $set: {
+      user: req.user,
+      date: req.body.date,
+      grateful: req.body.grateful,
+      greatness: req.body.greatness,
+      affirmation: req.body.affirmation
+    }
+  };
+
+  let options = {upsert: true, returnNewDocument: false, new: true};
+
+  Day
+    .findOneAndUpdate(query, updates, options)
     .then(day => res.status(201).json(day))
     .catch(err => {
       console.error(err);
       res.status(500).json({ message: "Internal server error" });
     });
+
 });
 
-// PUT - Update 
-// No validation needed?
-// IMPROVEMENT - But if nothing is sent in the one of the lists, don't save that in the document?
-router.put('/:id', jwtAuth, (req, res) => {
-	Day.update(
-		{"_id": req.params.id},
-		{
-			$set: {
-				grateful: req.body.grateful,
-				greatness: req.body.greatness,
-				affirmation: req.body.affirmation
-			}
-		}
-	)
-	.then(day => res.status(201).json(day))
-	.catch(err => {
-      console.error(err);
-      res.status(500).json({ message: "Internal server error" });
-    });
-});
 
 
 // DELETE - No need for this at MVP stage
